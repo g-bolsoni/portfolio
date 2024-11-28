ddeb2f228cee5075337efe155f8d23f1a34773a6
g-bolsoni <giovanebolsoni2@gmail.com>
2024-11-28 17:06:15 -0300

    Fix: change the point at which animation begins

--- app/components/github-repos.tsx
+++ app/components/github-repos.tsx
@@ -34,7 +34,7 @@ export default function GithubRepos() {
         <motion.div
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
+          viewport={{ once: true, amount: 0 }}
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
         >
           {repos.map((repo: GithubRepo, index) => (
